86eda7a8eb0a2aa7540d5efac0058a5c00d0c668
imraghavbnsal007 <raghavb025@gmail.com>
2022-04-27 17:21:32 +0100

    Audio added for creativity

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -1,7 +1,10 @@
 package ie.tudublin;
 
 import java.util.ArrayList;
-
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
 import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
@@ -9,14 +12,24 @@ import processing.data.TableRow;
 public class NematodeVisualiser extends PApplet
 {
 	ArrayList<Nematode> nematodes = new ArrayList<Nematode>();
+	Minim minim;
+    AudioPlayer ap;
+    AudioInput ai;
+    AudioBuffer ab;
+
 
 	public void keyPressed()
 	{		
 		if (keyCode == LEFT)
 		{
+
+		}
+		if (keyCode == RIGHT)
+		{
+
 		}		
 	}
-
+	
 
 	public void settings()
 	{
@@ -25,6 +38,11 @@ public class NematodeVisualiser extends PApplet
 
 	public void setup() 
 	{
+		minim = new Minim(this);
+        
+        ap = minim.loadFile("nucleya.mp3", 1024);
+        ap.play();
+        
 		colorMode(HSB);
 		
 		background(0);
