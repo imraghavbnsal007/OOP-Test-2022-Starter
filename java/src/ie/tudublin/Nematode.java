8a89a585511ebc6a96d088b375dd377e66d1b0f2
imraghavbnsal007 <raghavb025@gmail.com>
2022-04-27 16:22:43 +0100

    Class created and paramenter, constructor, get-set and tstring ready

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -4,7 +4,62 @@ public class Nematode {
     public String name;
     public float length;
     public boolean limbs;
-    public float gender;
-    public float eyes;
+    public String gender;
+    public boolean eyes;
+
+    public Nematode(String name, float length, boolean limbs, String gender, boolean eyes) {
+        this.name = name;
+        this.length = length;
+        this.limbs = limbs;
+        this.gender = gender;
+        this.eyes = eyes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public boolean isLimbs() {
+        return limbs;
+    }
+
+    public void setLimbs(boolean limbs) {
+        this.limbs = limbs;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public boolean isEyes() {
+        return eyes;
+    }
+
+    public void setEyes(boolean eyes) {
+        this.eyes = eyes;
+    }
+
+    @Override
+    public String toString() {
+        return "Nematode [eyes=" + eyes + ", gender=" + gender + ", length=" + length + ", limbs=" + limbs + ", name="
+                + name + "]";
+    }
+    
 
 }
